1fe33bc039c28adbb2698b056020d0b105d6fd30
Mburu-Waa-Waanjiku <108751464+Mburu-Waa-Waanjiku@users.noreply.github.com>
2022-12-07 21:00:15 +0300

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,9 @@ import { SnackbarProvider } from 'notistack';
 import { useEffect } from 'react';
 import { StoreProvider } from '../utils/Store';
 import { Analytics } from '@vercel/analytics/react';
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
