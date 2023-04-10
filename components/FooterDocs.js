85c051b34dc2571db9c2f51ed848061006651b7a
Mburu-Waa-Waanjiku <108751464+Mburu-Waa-Waanjiku@users.noreply.github.com>
2023-04-10 17:02:31 +0300

    Update FooterDocs.js

--- components/FooterDocs.js
+++ components/FooterDocs.js
@@ -62,7 +62,7 @@ function FooterDocs() {
                     <HeadsetMicRounded style={{marginRight:10}}/> Contact Us
                   </div>
                 </a>
-                <Link href="https://wa.me/254770097070?text=Hello,%20I'm%20Jane.I'd%20like%20your%20help%20....">
+                <Link href="https://wa.me/254103477957?text=Hello,%20I'm%20Jane.I'd%20like%20your%20help%20....">
                   <div style={{display: 'inline-flex', lineHeight: 2, borderRadius: 3, padding: '5px 20px' , background: 'rgb(34, 34, 34)', color:'white'}}>
                       <i style={{color:"white", marginRight:10, fontSize:"25px", }} className="fa fa-whatsapp whatsapp-icon"></i> Whatsapp Us
                   </div>
