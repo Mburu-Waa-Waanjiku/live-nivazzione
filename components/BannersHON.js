eeefe6d3d8fcdfd06b866c9b89c08f9e789d7225
Mburu-Waa-Waanjiku <108751464+Mburu-Waa-Waanjiku@users.noreply.github.com>
2023-04-28 10:33:54 +0300

    Add files via upload

--- components/BannersHON.js
+++ components/BannersHON.js
@@ -2,12 +2,19 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { debounce } from '../utils/helpers';
 import Link from 'next/link';
+import { Navigation, FreeMode, Thumbs, Pagination, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-function BannersHON({categs, banner, mylink, Anclets, Earrings, Necklaces }) {
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+function BannersHON({collections, categs, banner, mylink, Anclets, Earrings, Necklaces }) {
     
-  const [Offers, setOffers] = useState("http://localhost:3000/offers");
-  const [Hot, setHot] = useState("http://localhost:3000/trending");
-  const [New, setNew] = useState("http://localhost:3000/new-products");
+  const [Offers, setOffers] = useState("http://www.shiglam.com/offers");
+  const [Hot, setHot] = useState("http://http://www.shiglam.com/trending");
+  const [New, setNew] = useState("http://http://www.shiglam.com/new-products");
   
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
@@ -34,7 +41,8 @@ function BannersHON({categs, banner, mylink, Anclets, Earrings, Necklaces }) {
           {Hot == mylink && <Image style={{visibility: visible ? 'visible' : 'hidden', transition: 'visibility .2s'}} className="bg-gray-100" width={1600} height={500} alt="Offers" src={banner[5].image[0]}></Image>}
           {Earrings == mylink && <Image style={{visibility: visible ? 'visible' : 'hidden', transition: 'visibility .2s'}} className="bg-gray-100" width={1600} height={500} alt="Offers" src={banner[6].image[0]}></Image>}          
           {Anclets == mylink && <Image style={{visibility: visible ? 'visible' : 'hidden', transition: 'visibility .2s'}} className="bg-gray-100" width={1600} height={500} alt="Offers" src={banner[7].image[0]}></Image>}          
-          {Necklaces == mylink && <Image style={{visibility: visible ? 'visible' : 'hidden', transition: 'visibility .2s'}} className="bg-gray-100" width={1600} height={500} alt="Offers" src={banner[8].image[0]}></Image>}          
+          {Necklaces == mylink && <Image style={{visibility: visible ? 'visible' : 'hidden', transition: 'visibility .2s'}} className="bg-gray-100" width={1600} height={500} alt="Offers" src={banner[8].image[0]}></Image>}    
+          {collections == mylink && <Image style={{visibility: visible ? 'visible' : 'hidden', transition: 'visibility .2s'}} className="bg-gray-100" width={1600} height={500} alt="Offers" src={banner[2].image[0]}></Image>}      
         </div>
         <div style={{ position: 'absolute', width: '100%', transform: 'translate(0px, -60%)', zIndex: 10, display: 'flex', justifyContent: 'center'}}>
           <div style={{ boxShadow: 'rgb(64 60 67 / 20%) 0px 2px 5px 1px', width: '90%', backgroundColor:'rgba(255, 255, 255, 0.9)', gap: 8, borderRadius: 30, padding: '5px 15px 2px 15px'}} className="flex justify-around bannerimg">
@@ -70,38 +78,83 @@ function BannersHON({categs, banner, mylink, Anclets, Earrings, Necklaces }) {
               </div>
             </Link>
             </> : 
-            <>
-            <Link href="/Anclets">
-              <div style={{ cursor: 'pointer', scale: Anclets == mylink ? '1.2' : '1', transform: Hot ? 'translate(0px, 1.4px)' : 'translate(0px, 0px)' }} >
-                <div style={{overflow: 'hidden'}} >
-                  <Image width={60}  height={60} className="bg-gray-100 p-2" alt="" src="https://res.cloudinary.com/dddx5qpji/image/upload/v1678549575/ancletsmin_pk4p2f.jpg"/>
-                </div>
-                <div style={{backgroundColor:'rgba(0, 0, 0, 0.2)', borderRadius: 50, fontSize: 15, transform: 'translate(0px, -108%)', color: Anclets == mylink ? 'white' : 'rgba(255, 255, 255, 0.7)', fontWeight: 600, height: 60, width: 60}} className="absolute grid justify-center content-center">
-                  <div> Anclets </div>
-                </div>
-              </div>
-            </Link>
-            <Link href="/Earrings">
-              <div style={{ cursor: 'pointer', scale: Earrings == mylink ? '1.2' : '1', transform: Offers ? 'translate(0px, 1.4px)' : 'translate(0px, 0px)' }} >
-                <div style={{borderRadius: 50}} >
-                  <Image width={60}  height={60} className="bg-gray-100" alt="" src="https://res.cloudinary.com/dddx5qpji/image/upload/v1678549970/il_570xN.3254524046_9p24_2_ghkimd.jpg"/>
-                </div>
-                <div style={{backgroundColor:'rgba(0, 0, 0, 0.2)', borderRadius: 50, fontSize: 15, transform: 'translate(0px, -108%)', color: Earrings == mylink ? 'white' : 'rgba(255, 255, 255, 0.7)', fontWeight: 600, height: 60, width: 60}} className="absolute grid justify-center content-center">
-                  <div> Earrings </div>
-                </div>
-              </div>
-            </Link>
-            <Link href="/Necklaces">
-              <div style={{ cursor: 'pointer', scale: Necklaces == mylink ? '1.2' : '1', transform: New ? 'translate(0px, 1.4px)' : 'translate(0px, 0px)' }} >
-                <div style={{borderRadius: 50 }} >
-                  <Image width={60}  height={60} className="bg-gray-100" alt="" src="https://res.cloudinary.com/dddx5qpji/image/upload/v1678542559/neclacesmin_2_pzh7tr.jpg"/>
-                </div>
-                <div style={{backgroundColor:'rgba(0, 0, 0, 0.2)', borderRadius: 50, fontSize: 15, transform: 'translate(0px, -108%)', color: Necklaces == mylink ? 'white' : 'rgba(255, 255, 255, 0.7)', fontWeight: 600, height: 60, width: 60}} className="absolute grid justify-center content-center">
-                  <div> Necklaces </div>
-                </div>
-              </div>
-            </Link>
-            </>}
+            <Swiper                    
+               breakpoints={{
+                 100: {
+                    slidesPerView: 2.8,
+                 },
+                 350: {
+                    slidesPerView: 3.3,
+                 },
+                 647: {
+                    slidesPerView: 4,
+                 }             
+               }}
+      
+                 modules={[FreeMode, Navigation, Pagination, Autoplay, Thumbs]}
+                 spaceBetween={10}           
+                 loop={false}
+                 navigation= {true}
+                 centeredSlides={false}
+                 style={{ maxWidth: 1100 }}
+
+             onSwiper={(swiper) => console.log(swiper)}
+             onSlideChange={() => console.log('slide change')}
+        
+             >
+              <SwiperSlide className="hidden sm:flex">
+              </SwiperSlide>
+              <SwiperSlide className="ml-2">
+                <Link href="/collections">
+                  <div style={{  minWidth: 75, cursor: 'pointer', scale: collections == mylink ? '1.1' : '1', transform: Hot ? 'translate(0px, 1.4px)' : 'translate(0px, 0px)' }} >
+                    <div style={{overflow: 'hidden'}} >
+                      <Image width={60}  height={60} className="bg-gray-100 p-2" alt="" src="https://res.cloudinary.com/dddx5qpji/image/upload/v1682536105/My-Favorite-Jewelry-Pieces_sdaxfr.jpg"/>
+                    </div>
+                    <div style={{backgroundColor:'rgba(0, 0, 0, 0.2)', borderRadius: 50, fontSize: 15, transform: 'translate(0px, -108%)', color: collections == mylink ? 'white' : 'rgba(255, 255, 255, 0.7)', fontWeight: 600, height: 60, width: 60}} className="absolute grid justify-center content-center">
+                      <div> Collect </div>
+                      <div className="text-center"> ions </div>
+                    </div>
+                  </div>
+                </Link>
+              </SwiperSlide>
+              <SwiperSlide>
+                <Link href="/Anclets">
+                  <div style={{ minWidth: 75, cursor: 'pointer', scale: Anclets == mylink ? '1.1' : '1', transform: Hot ? 'translate(0px, 1.4px)' : 'translate(0px, 0px)' }} >
+                    <div style={{overflow: 'hidden'}} >
+                      <Image width={60}  height={60} className="bg-gray-100 p-2" alt="" src="https://res.cloudinary.com/dddx5qpji/image/upload/v1678549575/ancletsmin_pk4p2f.jpg"/>
+                    </div>
+                    <div style={{backgroundColor:'rgba(0, 0, 0, 0.2)', borderRadius: 50, fontSize: 15, transform: 'translate(0px, -108%)', color: Anclets == mylink ? 'white' : 'rgba(255, 255, 255, 0.7)', fontWeight: 600, height: 60, width: 60}} className="absolute grid justify-center content-center">
+                      <div> Anclets </div>
+                    </div>
+                  </div>
+                </Link>
+              </SwiperSlide>
+              <SwiperSlide>
+                <Link href="/Earrings">
+                  <div style={{ minWidth: 75, cursor: 'pointer', scale: Earrings == mylink ? '1.1' : '1', transform: Offers ? 'translate(0px, 1.4px)' : 'translate(0px, 0px)' }} >
+                    <div style={{borderRadius: 50}} >
+                      <Image width={60}  height={60} className="bg-gray-100" alt="" src="https://res.cloudinary.com/dddx5qpji/image/upload/v1678549970/il_570xN.3254524046_9p24_2_ghkimd.jpg"/>
+                    </div>
+                    <div style={{backgroundColor:'rgba(0, 0, 0, 0.2)', borderRadius: 50, fontSize: 15, transform: 'translate(0px, -108%)', color: Earrings == mylink ? 'white' : 'rgba(255, 255, 255, 0.7)', fontWeight: 600, height: 60, width: 60}} className="absolute grid justify-center content-center">
+                      <div> Earrings </div>
+                    </div>
+                  </div>
+                </Link>
+              </SwiperSlide>
+              <SwiperSlide>
+                <Link href="/Necklaces">
+                  <div style={{ minWidth: 75, cursor: 'pointer', scale: Necklaces == mylink ? '1.1' : '1', transform: New ? 'translate(0px, 1.4px)' : 'translate(0px, 0px)' }} >
+                    <div style={{borderRadius: 50 }} >
+                      <Image width={60}  height={60} className="bg-gray-100" alt="" src="https://res.cloudinary.com/dddx5qpji/image/upload/v1678542559/neclacesmin_2_pzh7tr.jpg"/>
+                    </div>
+                    <div style={{backgroundColor:'rgba(0, 0, 0, 0.2)', borderRadius: 50, fontSize: 15, transform: 'translate(0px, -108%)', color: Necklaces == mylink ? 'white' : 'rgba(255, 255, 255, 0.7)', fontWeight: 600, height: 60, width: 60}} className="absolute grid justify-center content-center">
+                      <div> Necklaces </div>
+                    </div>
+                  </div>
+                </Link>
+              </SwiperSlide>
+            </Swiper>
+            }
           </div>
         </div>
       </div>
