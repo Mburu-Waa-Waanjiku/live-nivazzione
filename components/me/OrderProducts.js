5cd59b949be59a7b8cd0b0df0b7dee523fd5231c
Mburu-Waa-Waanjiku <108751464+Mburu-Waa-Waanjiku@users.noreply.github.com>
2023-05-05 19:08:13 +0300

    Add files via upload

--- components/me/OrderProducts.js
+++ components/me/OrderProducts.js
@@ -9,13 +9,12 @@ function OrderProducts({ product}) {
           <Image
             width={364}
             height={484}
-            src={product.image && product.image[0]}
+            src={product.image && product.image[0].item}
             alt={product.name}
             className="shadow bg-gray-100 object-cover h-auto w-100"
             style={{borderRadius:20, backgroundColor: '#f3f4f6', overflow:"hidden"}}
           />
         </a>
-
         <div className="flex justify-between px-2 gap-3">
          <p className="desc price">Ksh{product.price}</p>
          <p style={{ fontWeight: 700, width: 85, transform: 'translate(10px, 6px)', fontSize: 14 }} className="flex"><div> O </div> <div className="adminInd"> rdered </div> <div> : {product.quantity}</div></p>
