6adf77915355575d6d97c2f72554738bb21660be
Mburu-Waa-Waanjiku <108751464+Mburu-Waa-Waanjiku@users.noreply.github.com>
2023-08-09 12:08:52 +0300

    Add files via upload

---
+++ pages/api/categories/index.js
@@ -0,0 +1,16 @@
+import nc from 'next-connect';
+import Banner from '../../../models/Banner';
+import db from '../../../utils/db';
+import { onError } from '../../../utils/error';
+
+const handler = nc({
+  onError,
+});
+
+handler.get(async (req, res) => {
+  await db.connect();
+  const banner = await Banner.find({ largeText1: "category"}).lean();
+  res.status(201).send(banner);
+});
+
+export default handler;
---
+++ pages/api/users/[id]/createShop.js
@@ -0,0 +1,55 @@
+import nc from 'next-connect';
+import Shop from '../../../../models/Shop';
+import { isAuth, isSeller } from '../../../../utils/auth';
+import db from '../../../../utils/db';
+import { onError } from '../../../../utils/error';
+import User from '../../../../models/User';
+import { signToken } from '../../../../utils/auth';
+
+const handler = nc({
+  onError,
+});
+handler.use(isAuth, isSeller);
+
+handler.post(async (req, res) => {
+    //console.log('starting');
+    await db.connect();
+      const newShop = new Shop({
+        ...req.body,
+        user: req.query.id,
+      });
+      //console.log(newShop);
+      const shop = await newShop.save();
+      //console.log('shop is');
+      //console.log(shop);
+      await User.updateOne(
+        { _id: req.query.id},
+        {
+          $set: {
+            'shopId': shop._id,
+          },
+        }
+      );
+      const user = await User.findOne({ _id: req.query.id });
+      //console.log('found user');
+    await db.disconnect();
+    if (user) {
+      const token = signToken(user);
+      const info = {
+        token,
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        isAffiliate: user.isAffiliate,
+        isSeller: user.isSeller,
+        shopId: user.shopId
+      };
+      res.send(info);
+      //console.log('we are done');
+    } else {
+      res.status(401).send({ message: 'Invalid user' });
+    }
+  });
+
+export default handler;
