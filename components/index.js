1ddde87ec0c3c69921e6fc5e058a9c2020f32949
Mburu-Waa-Waanjiku <108751464+Mburu-Waa-Waanjiku@users.noreply.github.com>
2023-08-18 08:54:55 +0300

    Delete components directory

--- components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Tabsottom } from './Tabsbottom';
-export { default as DealsCards } from './DealsCards';
-export { default as YourFoto } from './YourFoto';
-export { default as Newpost } from './Newpost';
